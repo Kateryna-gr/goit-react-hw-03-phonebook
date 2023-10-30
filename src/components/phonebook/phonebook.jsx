c4ad41814acee13a0b5aca8a038f9354da4be0bb
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-10-30 19:42:24 +0200

    task 1

--- src/components/phonebook/phonebook.jsx
+++ src/components/phonebook/phonebook.jsx
@@ -8,13 +8,28 @@ import { Filter } from 'components/filter/filter';
 export class Phonebook extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const currentContacts = localStorage.getItem('contacts');
+    if (currentContacts) {
+      this.setState({
+        contacts: JSON.parse(currentContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = values => {
     let check = this.state.contacts.find(
